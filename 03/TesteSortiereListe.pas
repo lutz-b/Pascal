db041525da26cf6b785d8cd29c0e14be2a5bd3c2
lutz-b <lutz.brandt@fairmas.com>
2017-05-14 23:35:43 +0200

    removed debug

--- 03/TesteSortiereListe.pas
+++ 03/TesteSortiereListe.pas
@@ -35,7 +35,6 @@ end; { GibListeAus }
 		while Zeiger^.next <> nil do
 		begin
 			GroessteZahl := Zeiger^.info;
-			{writeln('1.while: ' , Zeiger^.info , ' - ', GroessteZahl);}
 			
 			if GroessteZahl < Zeiger^.next^.info then	{bereits sortiert}
 			begin
@@ -49,14 +48,9 @@ end; { GibListeAus }
 				{finde stelle zum einfuegen}
 				while HilfsZeiger^.info < Zeiger^.next^.info do
 				begin
-					{writeln('2.while: ' , HilfsZeiger^.info);}
-					{readln();}
 					ZeigerEinfuegen := HilfsZeiger; {ZeigerEinfuegen ist immer auf dem vorherigen Element vor Hilfsteiger}
 					HilfsZeiger := HilfsZeiger^.next;
 				end; {while HilfsZeiger^.info < GroessteZahl}
-				
-				{writeln('Umhaengen: ' , ZeigerEinfuegen^.info, ' - ',HilfsZeiger^.info, ' - ', Zeiger^.next^.info);}
-				{readln();}
 				
 				if HilfsZeiger = ZeigerEinfuegen then {falls am Anfang eingefügt wird}
 				begin
@@ -73,10 +67,6 @@ end; { GibListeAus }
 					Zeiger^.next := Zeiger^.next^.next;
 					ZeigerEinfuegen^.next^.next := HilfsZeiger;
 				end;
-				
-				
-				{writeln('Umhaengen ende!');}
-				{GibListeAus(ioRefListe);}
 				
 			end; {if GroessteZahl < Zeiger^.next^.info}
 		end {while Zeiger^.next <> nil}
